85605ccff78ce02bb2a6b35fa0e6ede4942fe0b3
Amy <amylrobertson@icloud.com>
2018-02-28 18:48:57 +0000

    Buttons added and functional.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,31 +10,40 @@ artist1 = Artist.new({
   "name" => "Pvris"
   })
 
-artist2 = Artist.new({
-  "name" => "Bombay Bicycle Club"
-  })
+  artist2 = Artist.new({
+    "name" => "Bombay Bicycle Club"
+    })
 
-artist1.save()
-artist2.save()
+    artist1.save()
+    artist2.save()
 
-album1 = Album.new ({
-  "title" => "All We Know Of Heaven, All We Need Of Hell",
-  "quantity" => "10",
-  "artist_id" => artist1.id,
-  "buy_price" => "8",
-  "sell_price" => "15"
-  })
+    album1 = Album.new ({
+      "title" => "All We Know Of Heaven, All We Need Of Hell",
+      "quantity" => "10",
+      "artist_id" => artist1.id,
+      "buy_price" => "8",
+      "sell_price" => "15"
+      })
 
-album2 = Album.new ({
-  "title" => "So Long, See You Tomorrow",
-  "quantity" => "25",
-  "artist_id" => artist2.id,
-  "buy_price" => "12",
-  "sell_price" => "20"
-  })
+      album2 = Album.new ({
+        "title" => "So Long, See You Tomorrow",
+        "quantity" => "25",
+        "artist_id" => artist2.id,
+        "buy_price" => "12",
+        "sell_price" => "20"
+        })
 
-album1.save()
-album2.save()
+        album3 = Album.new({
+          "title" => "White Noise",
+          "quantity" => "4",
+          "artist_id" => artist1.id,
+          "buy_price" => "8",
+          "sell_price" => "15"
+          })
 
-binding.pry
-nil
+        album1.save()
+        album2.save()
+        album3.save()
+
+        binding.pry
+        nil
